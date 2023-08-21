04563627b8c4c5b55af2ba2c674fe51c5361ab47
Hamza9-86 <hamzazafar013@gmail.com>
2023-08-21 22:06:43 +0530

    cors updated

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ app.use(cookieParser());
 app.use(
   cors({
     credentials: true,
-    origin: "http://localhost:3000",
+    origin: "https://social-media-client-4sgi.onrender.com",
     optionsSuccessStatus: 200,
   })
 );
